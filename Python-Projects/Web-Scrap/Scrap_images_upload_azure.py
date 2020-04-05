8ec93100d798e950058e5dd84c623c8b86f09ac1
Thulasiram Bandaru <tbandaru@oradev.oraclecorp.com>
2020-04-05 15:12:19 +0530

    'web-scar_files'

--- Python-Projects/Web-Scrap/Scrap_images_upload_azure.py
+++ Python-Projects/Web-Scrap/Scrap_images_upload_azure.py
@@ -1,1 +1,54 @@
-test
+from azure.storage.blob import BlockBlobService, PublicAccess 
+from azure.storage.blob.models import Blob, ContentSettings
+import urllib.request
+import os
+import re
+import json
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+def run_sample(dayInterval):
+
+    block_blob_service = BlockBlobService(account_name='pilogimages', account_key='cIOZ9+d9nd7oEsfJ4IlnDSUZwWY09bf37n0ibXmNrFUI7PnIwcCBtQyAo13QFM85IBCdtDvTBQ9lUZpFRLBGhQ==')
+    container_name ='images'
+
+    
+    response = urllib.request.urlopen("http://<hostname>:<port>/Images")
+    #path = 'test-upload.jpg'
+    #block_blob_service.create_blob_from_path(container_name, path, path)
+    
+    soup = BeautifulSoup(response, features="html.parser")
+    soup.unicode
+    rows = soup.find_all('tr')
+        
+    for row in rows:    
+        data = row.find_all("td")
+        if len(data)>0:    
+            modDate = data[1].get_text()
+            
+            now = datetime.now()
+            date_format = "%b %d, %Y %H:%M:%S"
+            currentDateStr = now.strftime("%b %d, %Y %H:%M:%S")
+            
+            imageModifiedDate = datetime.strptime(modDate, date_format)
+            currentDate = datetime.strptime(currentDateStr, date_format)
+            
+            difference = currentDate - imageModifiedDate           
+
+            if difference.days < dayInterval:
+                name = data[0].find('a').get_text()
+                imgSize = data[2].get_text()
+                  
+                if imgSize != '--':
+                    resource = urllib.request.urlopen("http://<hostname>:<port>/Images/"+name)
+                    output = open(name,"wb")
+                    output.write(resource.read())
+                    output.close()
+                    block_blob_service.create_blob_from_path(container_name, name, name, 
+                                                            content_settings=ContentSettings(content_type="image/jpg"))
+                    os.remove(name)
+                
+    
+   # Main method.
+if _name_ == '_main_':
+    run_sample(10
--- Python-Projects/Web-Scrap/Srap-new-job-listings.py
+++ Python-Projects/Web-Scrap/Srap-new-job-listings.py
@@ -1,1 +1,99 @@
-test
+import datetime
+import requests
+from bs4 import BeautifulSoup, NavigableString, Tag
+import pandas as pd		
+
+# INPUTS / PATTERN for the scrapping
+
+inputData = {"mudah": {"site_url":"https://www.mudah.my/malaysia/Jobs-available-7020", "section":"date_job", 
+						"job_name": {"tag":"a", "tag_attr":"job_blk job_f"}, 
+						"job_salary":{"tag":"div", "tag_attr":"red_price job_f", "custom_processing":False}, 
+						"job_posted":{"tag":"span","tag_attr":"website job_ww job_f","custom_processing":True}
+						},
+			 "fastjobs": {"site_url":"https://www.fastjobs.my/malaysia-jobs/", "section":"job-post", 
+						"job_name": {"tag":"h2", "tag_attr":"visible-xs"}, 
+						"job_salary":{"tag":['span','strong'], "tag_attr":"text-primary", "custom_processing":True}, 
+						"job_posted":{"tag":"p","tag_attr":"coyinfo","custom_processing":True}
+						}
+			}
+
+
+def jobListings():
+	for idata in inputData:
+
+		jobName = [] 
+		postedDate = [] 
+		jobSalary = []
+
+		data=requests.get(inputData[idata]['site_url'])
+		results = BeautifulSoup(data.content, features="html.parser", from_encoding="iso-8859-1")
+
+		for res in results.findAll('div', attrs={'class':inputData[idata]['section']}):
+
+			if inputData[idata]['job_posted']['custom_processing']:
+				postedVal = res.find(inputData[idata]['job_posted']['tag'], attrs={'class':inputData[idata]['job_posted']['tag_attr']})
+				currentDate = getCurrentDate(siteName=idata, postedVal=postedVal)
+			else:
+				currentDate = res.find(inputData[idata]['job_posted']['tag'], attrs={'class':inputData[idata]['job_posted']['tag_attr']})
+
+			if currentDate:
+
+				name = res.find(inputData[idata]['job_name']['tag'], attrs={'class':inputData[idata]['job_name']['tag_attr']}).text
+
+				if inputData[idata]['job_salary']['custom_processing']:
+					salaryVal = res.findAll(inputData[idata]['job_salary']['tag'], attrs={'class':inputData[idata]['job_salary']['tag_attr']})
+					salary = None
+					salaryResult = getSalary(siteName=idata, salaryVal=salaryVal)
+					if salaryResult:
+						salary = salaryResult.text
+				else:
+					salary = res.find(inputData[idata]['job_salary']['tag'], attrs={'class':inputData[idata]['job_salary']['tag_attr']}).text		
+
+				jobName.append(name)
+				postedDate.append(currentDate)
+				jobSalary.append(salary)
+
+		
+			dframe = pd.DataFrame({'Job Title':jobName,'Salary':jobSalary,'Posted':postedDate}) 
+			dframe.to_csv(idata+'.csv', index=False, encoding='utf-8')
+
+
+
+# Utility Methods
+
+def getCurrentDate(siteName=None, postedVal=None):
+	""" This method will parse the input and return the current date """
+	result = None
+	if siteName == "mudah":
+		if postedVal:
+			if "Today" in postedVal.text:
+				result = datetime.datetime.today().date()
+	elif siteName == "fastjobs":
+		if postedVal:
+			for posted in postedVal:
+				if isinstance(posted, NavigableString):
+					dateval = str(posted).replace('Posted on ','').strip()		
+					datestr = dateval+' '+str(datetime.date.today().year) # The date - 29 Dec 2017
+					formatstr = '%d %b %Y' # The format
+					datetimeobj = datetime.datetime.strptime(datestr, formatstr)
+					if datetime.datetime.today().date() == datetimeobj.date():
+						result = datetimeobj.date()
+	return result
+				
+
+def getSalary(siteName=None, salaryVal=None):
+	""" This method will parse the input and return the salary """
+	result = None
+	if siteName == "mudah":
+		if "Today" in postedVal.text:
+			pass
+	elif siteName == "fastjobs":
+		if salaryVal:
+			result = salaryVal[0].find('strong')
+	return result
+
+
+if __name__ == '__main__':
+	jobListings()
+		
+		
