52201008c28afabca36d1a364bcb829fa044ab74
Thulasiram Bandaru <tbandaru@oradev.oraclecorp.com>
2020-04-05 13:23:52 +0530

    'deleted_redundant_dir'

--- PHP-Project/sunsms_new/application/views/config/config.php
+++
@@ -1,1 +0,0 @@
-<?phpdefine('ERROR_HTML', '<div class="alert alert-danger" style="display:none;"><strong>Alert!</strong><span id="err_valid_msg"> Indicates a dangerous or potentially negative action.</span></div>')															?>
